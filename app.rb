2492d063ef91f37b6b6b98d269742d56c31d38fc
RyleeBerger <ryleehaddon24@outlook.com>
2018-06-12 17:47:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+ 
+  get '/name' do
+    "My name is Rylee"
+  end
+  
+  get '/hometown'
+  "My hometown is Denver"
 end
+
+  get '/favorite-song'
+" My favorite song is I'm on My Way, anything by Imagine Dragons, Metallica, Led Zepplin, or Grateful Dead"
+end
+
+end
+
