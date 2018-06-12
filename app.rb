2e960eaba954c68747fb293aee0a427f270ea538
RyleeBerger <ryleehaddon24@outlook.com>
2018-06-12 17:52:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+ 
+  get '/name' do
+    "My name is Rylee"
+  end
+  
+  get '/hometown' do
+  "My hometown is Denver, Colorado"
 end
+
+  get '/favorite-song' do 
+" My favorite song is I'm on My Way, anything by Imagine Dragons, Metallica, Led Zepplin, or Grateful Dead"
+end
+
+end
+
